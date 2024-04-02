08c1cf773c66bb8c8dcf8cb1b9e3b09262bb67f6
Andy <alexandru.flecescu@gmail.com>
2024-04-02 22:11:14 +0200

    Update macOS icon in starship.zsh

--- dotfiles/zsh/zsh-config/starship.zsh
+++ dotfiles/zsh/zsh-config/starship.zsh
@@ -40,7 +40,7 @@ case $_distro in
     *devuan*)                ICON="";;
     *manjaro*)               ICON="";;
     *rhel*)                  ICON="";;
-    *macos*)                 ICON="";;
+    *macos*)                 ICON="";;
     *)                       ICON="";;
 esac
 
